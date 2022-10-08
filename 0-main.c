304294a06e08b6361cec355c1e0d35e570bccd4c
Jockthan <jasminblinkz@gmail.com>
2022-10-08 14:09:36 -0700

    task

--- 0-main.c
+++ 0-main.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 
-char *toks_num = NULL;
+global_t global = {EXIT_SUCCESS, NULL};
 
 /**
  * main - starting point for monty interpreter
@@ -14,7 +14,6 @@ char *toks_num = NULL;
 int main(int argc, char *argv[])
 {
 	FILE *monty_fd = NULL;
-	int exit_code = EXIT_SUCCESS;
 
 	if (argc != 2)
 	{
@@ -22,15 +21,15 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	monty_fd = fopen(argv[1], "r");
-	
+
 	if (!monty_fd)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	
-	exit_code = exec_monty(monty_fd);
+
+	global.err_status = exec_monty(monty_fd);
 	fclose(monty_fd);
 
-	return (exit_code);
+	exit(global.err_status);
 }
